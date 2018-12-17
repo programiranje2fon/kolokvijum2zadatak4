21cf4c648f0a527e7a5b4590dba0375df14f9f49
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-17 12:41:42 +0100

    Dodat tekst sa resenjima.

--- src/grad/exception/GradException.java
+++ src/grad/exception/GradException.java
@@ -2,6 +2,8 @@ package grad.exception;
 
 public class GradException extends RuntimeException {
 
+	private static final long serialVersionUID = 1L;
+
 	public GradException(String message) {
 		super(message);
 	}
