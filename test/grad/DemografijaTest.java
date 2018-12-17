ef6fa7553b4df8aab6da28d16d9832de00d3c89d
Nikola Milikic <nikola.milikic@gmail.com>
2018-12-17 13:49:28 +0100

    Azurirani testovi.

--- test/grad/DemografijaTest.java
+++ test/grad/DemografijaTest.java
@@ -9,12 +9,12 @@ import test.TestUtil;
 public class DemografijaTest {
 
 	@Test
-	public void metoda_ucitajFestivaleIzFajla() {
+	public void metoda_dodajGrad() {
 		assertTrue("U interfejsu nije definisana metoda dodajGrad(Grad)", TestUtil.doesMethodExist(Demografija.class, "dodajGrad", new Class<?>[]{Grad.class}));
 	}
 	
 	@Test
-	public void metoda_objediniArhive() {
+	public void metoda_upisiGradoveUKategorije() {
 		assertTrue("U interfejsu nije definisana metoda upisiGradoveUKategorije()", TestUtil.doesMethodExist(Demografija.class, "upisiGradoveUKategorije", new Class<?>[]{}));
 	}
 }
